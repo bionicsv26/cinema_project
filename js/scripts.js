5f6a7cad8fcd49042db6ffe937c671262b38b3f6
Sergey Kucherenko <skv026@bk.ru>
2026-05-19 09:18:23 +0300

    Добавлен адаптив и анимация

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 let cost = 800;
 let totalPrice = 0;
+
 schemeSvg.addEventListener('click', (event) => {
     if (!event.target.classList.contains('seat-booked')) {
         event.target.classList.toggle('seat-active');
@@ -10,3 +13,8 @@ schemeSvg.addEventListener('click', (event) => {
         totalPriceTag.textContent = totalPrice;
     }
 });
+
+menuButton.addEventListener('click', (event) => {
+    menu.classList.toggle('is-open');
+
+})
